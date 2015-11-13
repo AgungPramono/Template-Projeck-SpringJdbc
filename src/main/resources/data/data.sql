3f8617d296bfd5fe52ba1085fb5557f191baafe5
Agung Pramono <agungpermadi13@gmail.com>
2015-11-13 23:15:52 +0700

    sample data

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,7 @@
+delete from biodata;
+
+insert into biodata (nama,alamat,tanggalLahir,email) values
+('biodata-test0','address-test0','2015-11-13','biodata-test0@gmail.com'),
+('Deni','kediri','2015-11-13','deni@gmail.com'),
+('diah','madiun','2015-11-14','diah@yahoo.com'),
+('beni','bandung','2015-09-29','beni@yahoo.com');
