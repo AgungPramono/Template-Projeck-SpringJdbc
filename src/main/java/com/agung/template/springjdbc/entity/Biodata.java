cf1055ea7e6636dd57e24b3ca4f6f4ae1f0cfcfb
Agung Pramono <agungpermadi13@gmail.com>
2015-11-13 23:19:53 +0700

    Class untuk struktur data

---
+++ src/main/java/com/agung/template/springjdbc/entity/Biodata.java
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (c) 2015 Agung Pramono <agungpermadi13@gmail.com || www.github.com/agung pramono>.
+ *  All rights reserved.
+ * 
+ * Silahkan digunakan dengan bebas / dimodifikasi
+ * Dengan tetap mencantumkan nama @author dan Referensi / Source
+ * Terima Kasih atas Kerjasamanya.
+ * 
+ *  Biodata.java
+ * 
+ *  Created on Nov 13, 2015, 9:09:09 PM
+ */
+package com.agung.template.springjdbc.entity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author agung
+ */
+public class Biodata {
+    private Integer id;
+    private String nama;
+    private String alamat;
+    private Date tanggalLahir;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
